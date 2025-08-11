e35d1cf25a616f398c0132cb2dc517b91f54c470
hxabcd <hxabcd1234@outlook.com>
2025-08-12 03:02:27 +0800

    feat: 增加友链

--- src/config.ts
+++ src/config.ts
@@ -46,6 +46,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
+		{
+			name: "友链",
+			url: "/friends/",
+			external: false,
+		},
 		// {
 		// 	name: "GitHub",
 		// 	url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
